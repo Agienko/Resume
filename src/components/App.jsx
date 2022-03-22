04e2035433e5fe1ec4cfd306f13287d6fa431b0b
Yuriy <Agienkoyura@gmail.com>
2022-03-22 14:56:12 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const App = props =>(
+    <div className="container">
+        <aside>
+            ФОТО
+            КОНТАКТЫ
+            ЯЗЫКИ
+            НАВЫКИ
+        </aside>
+        <main>
+            ШАПКА
+            ОПЫТ РАБОТЫ
+            ОБРАЗОВАНИЕ
+            КУРСЫ
+            О СЕБЕ
+        </main>
+
+
+
+    </div>
+   
+)
+
+export default App
